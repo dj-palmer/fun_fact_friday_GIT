fe810d444728b7f03b5b8a4de8e0624ed4fa628f
Dave P <david.palmer@mariestopes.org.uk>
2016-06-02 19:55:07 +0100

    well, now dawg

--- facts/urls.py
+++ facts/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from views import hello
 
 urlpatterns = [
-    url(r'^hello/(?P<name>[-\w]+)$', hello)
+    url(r'^hello/$', hello),
+    url(r'^hello/(?P<name>[-\w]?)$', hello)
 ]
--- facts/views.py
+++ facts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request, name='dawg'):
+def hello(request, name="dawg"):
 	import pdb
 	#pdb.set_trace()
 	return HttpResponse("Yo %s!" %name) 
