355a5c22b1c5e073d70f8b4d7653440ab11bc069
Dave P <david.palmer@mariestopes.org.uk>
2016-06-02 20:45:01 +0100

    working getFact displaying a random fact to the browser

--- facts/models.py
+++ facts/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Fact(models.Model):
+	short_text = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return "%s" %self.short_text
--- facts/views.py
+++ facts/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+#Importing our fact model into the views
+
+#Imports EVERYTHING in models.py
+##from facts import models
+
+##Cleaner -- imports only the class we are after (just so happens we only have 1!)
+from facts.models import Fact
+import random
+
 # Create your views here.
-def hello(request, name="dawg"):
+def getFact(request):
 	import pdb
 	#pdb.set_trace()
-	return HttpResponse("Yo %s!" %name) 
+	return HttpResponse(Fact.objects.get(id=random.randint(1,Fact.objects.count())))
