7c435c1130c8faca761df6652f3ab79bc29dd919
Dave P <david.palmer@mariestopes.org.uk>
2016-06-03 12:31:12 +0100

    updated models to include a reference link for facts, and allowed users to choose to get more facts or learn about the fact via the link

--- facts/urls.py
+++ facts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from views import getFact, getFacts
 
 urlpatterns = [
-    url(r'^$', getFact),
+    url(r'^$', getFact, name="fact"),
     url(r'^morefacts$', getFacts)
 ]
