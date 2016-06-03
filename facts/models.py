b2063694c35d471fbdd656ae7f77607ca754678b
David Downes <sourcetree@theukdave.com>
2016-06-03 12:45:05 +0100

    whitespace changes only for conformation and pep8

--- facts/models.py
+++ facts/models.py
@@ -2,10 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-class Fact(models.Model):
-	short_text = models.CharField(max_length=255)
-	ref_link = models.CharField(max_length=1000, blank=True, null=True)
 
-	def __unicode__(self):
-		return "%s" %self.short_text
+class Fact(models.Model):
+    short_text = models.CharField(max_length=255)
+    ref_link = models.CharField(max_length=1000, blank=True, null=True)
+
+    def __unicode__(self):
+        return "%s" % self.short_text
--- facts/views.py
+++ facts/views.py
@@ -4,26 +4,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-#Importing our fact model into the views
+# Importing our fact model into the views
 
-#Imports EVERYTHING in models.py
-##from facts import models
+# Imports EVERYTHING in models.py
+# from facts import models
 
-##Cleaner -- imports only the class we are after (just so happens we only have 1!)
+# Cleaner -- imports only the class we are after
 from facts.models import Fact
 
-# Create your views here.
+
 def getFact(request):
-	import pdb
-	#pdb.set_trace()
-	fact=Fact.objects.get(id=random.randint(1,Fact.objects.count()))
-	template = loader.get_template('facts/index.html')
-	context = {"fact" : fact}
-	return HttpResponse(template.render(context,request))
+    fact = Fact.objects.get(id=random.randint(1, Fact.objects.count()))
+    template = loader.get_template('facts/index.html')
+    context = {"fact": fact}
+    return HttpResponse(template.render(context, request))
+
 
 def getFacts(request):
-	facts=Fact.objects.all()
-	template = loader.get_template('facts/facts.html')
-	context = {"facts" : facts}
-	return HttpResponse(template.render(context,request))
-
+    facts = Fact.objects.all()
+    template = loader.get_template('facts/facts.html')
+    context = {"facts": facts}
+    return HttpResponse(template.render(context, request))
